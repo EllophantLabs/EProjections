2a805272dab08000f590ced50783c22047eb0883
aCodingCube <lorenzwittmann2008@gmail.com>
2026-04-30 18:56:59 +0200

    added update-version.js

---
+++ update-version.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+const newVersion = process.env.npm_package_version;
+
+const filesToUpdate = [
+  {
+    path: "./updater.json",
+    pattern: /"version": ".*?"/g,
+    replacement: `"version": "${newVersion}"`,
+  },
+  {
+    path: "./src-tauri/tauri.conf.json",
+    pattern: /"version": ".*?"/,
+    replacement: `"version": "${newVersion}"`,
+  },
+  {
+    path: "./StartingWindow/index.html",
+    pattern: /v\d+\.\d+\.\d+/,
+    replacement: `v${newVersion}`,
+  },
+];
+
+filesToUpdate.forEach((file) => {
+  if (fs.existsSync(file.path)) {
+    let content = fs.readFileSync(file.path, "utf8");
+    content = content.replace(file.pattern, file.replacement);
+    fs.writeFileSync(file.path, content);
+    console.log(`Updated ${file.path} to ${newVersion}`);
+  }
+});
