f1ba07a64ae258f690e185a1beae98d5af2d7a46
aCodingCube <lorenzwittmann2008@gmail.com>
2026-06-02 17:54:36 +0200

    added basic interface

--- src/MainWindow/transitionInterface.ts
+++ src/MainWindow/transitionInterface.ts
@@ -1,8 +1,33 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    alert("Test Transition Interface.ts!");
-});
+declare global {
+    interface Window {
+        __TAURI__: any;
+    }
+}
 
-function transitionCMD()
+const { emit } = window.__TAURI__.event;
+
+export function preloadMedia(src: string, isVideo: boolean, isLooped: boolean) {
+    emit("preloadMedia", { src: src, isVideo: isVideo, isLooped: isLooped });
+}
+
+export function preloadColor(src: string) {
+    emit("preloadColor", { src: src });
+}
+
+export function transitionCMD(src: string, transtionDuration: number, transition: boolean) {
+    emit("transitionCMD", { src: src, transitionDuration: transtionDuration, transition: transition });
+}
+
+export function blackOut(transition: boolean) {
+    emit("blackOut", { transition: transition });
+}
+
+export function updateIsLooped(src:string, isLooped: boolean)
 {
-    
+    emit("updateIsLooped",{src:src, isLooped: isLooped});
+}
+
+export function updateDuration(src:string, transitionDuration: number)
+{
+    emit("updateIsLooped",{src:src, transitionDuration: transitionDuration});
 }
--- src/SecWindow/transition.ts
+++ src/SecWindow/transition.ts
@@ -1,3 +1,36 @@
-window.addEventListener("DOMContentLoaded", () => {
-    alert("Mega transitions werden hier erstellt! :)");
-})
+const { listen } = window.__TAURI__.event;
+
+// active src
+export let currentSrc: string = "";
+export let currentLooped: boolean = false;
+export let currentDuration: number = 0;
+
+listen("preloadMedia", (event: { payload: { src: string, isVideo: boolean, isLooped: boolean } }) => {
+
+});
+
+listen("preloadColor", (event: { payload: { src: string } }) => {
+
+});
+
+listen("transitionCMD", (event: { payload: { src: string, transitionDuration: number, transition: boolean } }) => {
+    currentSrc = event.payload.src;
+});
+
+listen("balackOut", (event: { payload: { transition: boolean } }) => {
+
+});
+
+listen("updateIsLooped", (event: { payload: { src: string, isLooped: boolean } }) => {
+    if (currentSrc != event.payload.src) {
+        return;
+    }
+    currentLooped = event.payload.isLooped;
+});
+
+listen("updateDuration", (event: { payload: { src: string, transitionDuration: number } }) => {
+    if (currentSrc != event.payload.src) {
+        return;
+    }
+    currentDuration = event.payload.transitionDuration;
+});
